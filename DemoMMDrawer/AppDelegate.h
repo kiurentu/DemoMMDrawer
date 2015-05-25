e0a9a844a6bc00b7a61991c58c3fe3575321870c
kiurentu <331059113@qq.com>
2015-05-25 18:07:24 +0800

    Initial Commit

---
+++ DemoMMDrawer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoMMDrawer
+//
+//  Created by Admin on 25/5/15.
+//  Copyright (c) 2015年 biz.ourmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
